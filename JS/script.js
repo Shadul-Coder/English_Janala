7cf872ecff8c78c7cf3916e9b4b09b87ab904a7e
Shadul-Coder <codershadul@gmail.com>
2025-09-05 12:57:37 +0600

    Worked On Learn Vocabularies Section

--- JS/script.js
+++ JS/script.js
@@ -16,9 +16,9 @@ const displayLesson = (levels) => {
   for (const level of levels) {
     let lessonButton = document.createElement(`div`);
     lessonButton.innerHTML = `
-      <button onclick="loadWord(${
-        level[`level_no`]
-      })" class="btn btn-soft btn-info hover:text-white active:text-white">
+      <button onclick="loadWord(${level[`level_no`]})" id="Lesson_Button-${
+      level[`level_no`]
+    }" class="Lesson_Button btn btn-soft btn-info hover:text-white active:text-white">
         <i class="fa-solid fa-book-open"></i> Lesson - ${level[`level_no`]}
       </button>
     `;
@@ -27,6 +27,13 @@ const displayLesson = (levels) => {
 };
 
 const loadWord = async (level) => {
+  let buttons = document.querySelectorAll(`.Lesson_Button`);
+  for (let button of buttons) {
+    button.classList.remove(`text-white`, `bg-[#0ca8e0]`);
+  }
+  document
+    .querySelector(`#Lesson_Button-${level}`)
+    .classList.add(`text-white`, `bg-[#0ca8e0]`);
   let container = document.querySelector(`#Word_Parent`);
   container.innerHTML = ``;
   loadingScreen(container);
@@ -57,13 +64,19 @@ const displayWord = (wordList) => {
     let box = document.createElement(`div`);
     box.className = `bg-white p-5 text-center rounded-xl space-y-3 sm:space-y-5`;
     box.innerHTML = `
-              <h1 class="mt-3 text-xl font-bold sm:mt-5 sm:text-2xl">${word.word}</h1>
+              <h1 class="mt-3 text-xl font-bold sm:mt-5 sm:text-2xl">${
+                word.word
+              }</h1>
               <p class="text-base sm:text-xl">Meaning/Pronunciation</p>
               <h3 class="Bangla-Font text-xl font-semibold sm:text-2xl">
-                ${word.meaning} / ${word.pronunciation}
+                ${word.meaning ? word.meaning : `অর্থ পাওয়া যায়নি`} / ${
+      word.pronunciation
+    }
               </h3>
               <div class="flex justify-between">
-                <button class="btn bg-[#1a91ff1a] border-none rounded-lg">
+                <button onclick="loadWordDetails(${
+                  word.id
+                })" class="btn bg-[#1a91ff1a] border-none rounded-lg">
                   <i class="fa-solid fa-circle-info"></i>
                 </button>
                 <button class="btn bg-[#1a91ff1a] border-none rounded-lg">
@@ -75,4 +88,52 @@ const displayWord = (wordList) => {
   }
   parent.innerHTML = ``;
   parent.appendChild(container);
+};
+
+const loadSynonyms = (synonyms) => {
+  if (synonyms.length === 0) {
+    return `<p class="Bangla-Font">কোনো সমার্থক শব্দ পাওয়া যায়নি</p>`;
+  }
+  const synonymList = synonyms.map(
+    (syn) =>
+      `<button class="btn bg-[#1a91ff1a] rounded-md font-normal text-[13px] sm:text-base">${syn}</button>`
+  );
+  return synonymList.join(` `);
+};
+
+const loadWordDetails = async (id) => {
+  let res = await fetch(`https://openapi.programming-hero.com/api/word/${id}`);
+  let json = await res.json();
+  displayWordDetails(json.data);
+};
+
+const displayWordDetails = (data) => {
+  let parent = document.querySelector(`#Modal_Content`);
+  parent.innerHTML = ``;
+  let content = document.createElement(`div`);
+  content.className = `border-1 border-[#edf7ff] p-5 rounded-xl space-y-3 sm:space-y-7`;
+  content.innerHTML = `
+                <h1 class="font-bold sm:text-2xl">
+                  ${data.word} (<i class="fa-solid fa-microphone-lines"></i> :
+                  <span class="Bangla-Font">${data.pronunciation}</span>)
+                </h1>
+                <div class="text-[13px] space-y-1 sm:text-lg sm:space-y-3">
+                  <h3 class="font-semibold">Meaning</h3>
+                  <h1 class="Bangla-Font">${
+                    data.meaning ? data.meaning : `অর্থ পাওয়া যায়নি`
+                  }</h1>
+                </div>
+                <div class="text-[13px] space-y-1 sm:text-lg sm:space-y-3">
+                  <h5 class="font-semibold">Example</h5>
+                  <p>${data.sentence}</p>
+                </div>
+                <div class="text-[13px] space-y-1 sm:text-lg sm:space-y-3">
+                  <h5 class="Bangla-Font font-semibold">সমার্থক শব্দগুলো</h5>
+                  <div class="flex flex-wrap gap-1 sm:gap-1.5">
+                    ${loadSynonyms(data.synonyms)}
+                  </div>
+                </div>
+  `;
+  parent.appendChild(content);
+  word_modal.showModal();
 };
